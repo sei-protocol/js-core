7041795314fdde58f14057f30560c71252f19547
Carson Aberle <carson@pineapplelabs.io>
2022-10-18 00:50:07 -0600

    Added readme copy to predeploy script

--- src/queryClient/queryClient.ts
+++ src/queryClient/queryClient.ts
@@ -1,6 +1,5 @@
 import { seiprotocol } from '@sei-js/proto';
 
 export const getQueryClient = async (restEndpoint: string) => {
-	const queryClient = await seiprotocol.ClientFactory.createLCDClient({ restEndpoint });
-	return queryClient;
+	return await seiprotocol.ClientFactory.createLCDClient({ restEndpoint });
 };
