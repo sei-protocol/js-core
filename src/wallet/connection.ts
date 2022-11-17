4d2e5fa0ebf7871b77fb036f54a026c17a036754
Carson Aberle <carson@pineapplelabs.io>
2022-11-17 13:23:44 -0700

    Adjusted connect function for coin98

--- src/wallet/connection.ts
+++ src/wallet/connection.ts
@@ -4,16 +4,20 @@ import { WalletConnect, WalletWindowKey } from './types';
 declare global {
 	interface Window {
 		keplr: { getOfflineSigner: (string) => Promise<any>; experimentalSuggestChain: (object) => void };
-		leap: { getOfflineSigner: (string) => Promise<any> };
+		leap: { getOfflineSigner: (string) => Promise<any>; experimentalSuggestChain: (object) => void };
+		coin98: { cosmos: (chain) => Promise<any> };
+		falcon: { getOfflineSigner: (string) => Promise<any>; experimentalSuggestChain: (object) => void };
 	}
 }
 
 export const connect = async (inputWallet: WalletWindowKey, chainId?: string, restUrl?: string, rpcUrl?: string): Promise<WalletConnect | undefined> => {
-	await window[inputWallet].experimentalSuggestChain(getChainSuggest(chainId, restUrl, rpcUrl));
+	const windowKey = inputWallet === 'coin98' ? 'keplr' : inputWallet;
+
+	await window[windowKey].experimentalSuggestChain(getChainSuggest(chainId, restUrl, rpcUrl));
 
 	if (typeof window === 'undefined' || !window) return;
 
-	const offlineSigner = await window[inputWallet].getOfflineSigner(chainId);
+	const offlineSigner = await window[windowKey].getOfflineSigner(chainId);
 	const accounts = await offlineSigner.getAccounts();
 
 	return { offlineSigner, accounts };
